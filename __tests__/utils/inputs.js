fafd5e6cb567a6dec514b3eeec2f9b46a0f023ab
Abhishek Saha <abhisheksaha11@gmail.com>
2018-11-20 16:08:25 +0100

    transformed to commonjs

--- __tests__/utils/inputs.js
+++ __tests__/utils/inputs.js
@@ -1,28 +1,28 @@
 const path = require("path");
 
 const installationPath = path.join(
-    __dirname,
-    "/../playground/installations/lp"
+  __dirname,
+  "/../playground/installations/lp"
 );
 const cachePath = path.join(__dirname, "../playground/cache");
 
 module.exports = {
-    installationPath,
-    cachePath,
-    setInstallDefaults: installer => {
-        installer.releases = [
-            { version: 1, zipball_url: "http://github.com/version/1" },
-        ];
-        installer.cachePath = cachePath;
-        installer.inputs.installationPath = installationPath;
-        installer.inputs.version = 1;
-        installer.inputs.rootUrl = "rootUrl";
-        installer.inputs.apiUrl = "apiUrl";
-        installer.inputs.uploadUrl = "uploadUrl";
-        installer.inputs.appPort = 50;
-        installer.inputs.apiPort = 100;
-        installer.foldersToLink = ["index.txt"];
+  installationPath,
+  cachePath,
+  setInstallDefaults: installer => {
+    installer.releases = [
+      { version: 1, zipball_url: "http://github.com/version/1" },
+    ];
+    installer.cachePath = cachePath;
+    installer.inputs.installationPath = installationPath;
+    installer.inputs.version = 1;
+    installer.inputs.rootUrl = "rootUrl";
+    installer.inputs.apiUrl = "apiUrl";
+    installer.inputs.uploadUrl = "uploadUrl";
+    installer.inputs.appPort = 50;
+    installer.inputs.apiPort = 100;
+    installer.foldersToLink = ["index.txt"];
 
-        return installer;
-    },
+    return installer;
+  },
 };
